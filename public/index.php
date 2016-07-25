f82a11094f62a2eadf4e845f9649371e2986e4d6
Anders Grendstadbakk <anders.2205@gmail.com>
2016-07-25 14:49:38 +0200

    Add whitelist functionality

--- public/index.php
+++ public/index.php
@@ -1,7 +1,17 @@
 <?php
 
-$config = file_get_contents(dirname(__FILE__) . '/../config.json');
+$script_path = dirname(__FILE__) . '/..';
+
+$config = file_get_contents($script_path . '/config.json');
 $config = json_decode($config, TRUE);
+
+if (file_exists($script_path . '/whitelist.json')) {
+  $whitelist = file_get_contents($script_path . '/whitelist.json');
+  $whitelist = json_decode($whitelist, TRUE);
+}
+else {
+  $whitelist = array();
+}
 
 if (isset($_GET['code'])) {
   $base_path = 'https://api.instagram.com/oauth/access_token/';
@@ -39,6 +49,13 @@ elseif (isset($_GET['error'])) {
   echo $_GET['error_description'];
 }
 else {
+  if (!empty($whitelist) && !match_that_path($_GET['site'], $whitelist)) {
+    header("HTTP/1.0 403 Forbidden");
+    echo 'Forbidden';
+    exit;
+  }
+
+
   $base_path = 'https://api.instagram.com/oauth/authorize/';
 
   $query = http_build_query(array(
@@ -48,7 +65,31 @@ else {
     'state' => $_GET['site']
   ));
 
-  $instagram_link = sprintf('%s?%s', $base_path, $query);
+  header('Location: ' . sprintf('%s?%s', $base_path, $query));
+}
 
-  header('Location: ' . $instagram_link);
+
+
+
+/**
+ * Shamelessly stolen from drupal_match_path().
+ */
+function match_that_path($path, $patterns) {
+  $patterns = implode("\n", $patterns);
+
+  // Convert path settings to a regular expression.
+  // Therefore replace newlines with a logical or, /* with asterisks and the <front> with the frontpage.
+  $to_replace = array(
+    '/(\r\n?|\n)/', // newlines
+    '/\\\\\*/', // asterisks
+    '/(^|\|)\\\\<front\\\\>($|\|)/' // <front>
+  );
+  $replacements = array(
+    '|',
+    '.*',
+  );
+  $patterns_quoted = preg_quote($patterns, '/');
+  $regexps = '/^(' . preg_replace($to_replace, $replacements, $patterns_quoted) . ')$/';
+
+  return (bool) preg_match($regexps, $path);
 }
